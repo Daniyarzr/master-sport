fce1ea6bee16b39932904c98f6e44b1d317279dc
ArtemkaPoleno <hdh16788@gmail.com>
2026-05-15 14:30:24 +0400

    Create LoginRequest.php

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, array<int, string>>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Укажите email.',
+            'email.email' => 'Некорректный email.',
+            'password.required' => 'Укажите пароль.',
+        ];
+    }
+}
